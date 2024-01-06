7343f4a7f4f020b7ec04a733b3c1fdd7ad2f393e
Bethwel Njenga <bethwelnjenga237@gmail.com>
2024-01-06 19:50:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.post("/stk",  async (req, res)=>{
   
   const password = new Buffer.from(shortcode + passkey).toString("base64");
 
+
   
    await axios.post(
     "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
